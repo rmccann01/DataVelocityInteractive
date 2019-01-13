4a8aaa4bb2ada6a1fa7aab1c04cd312024e1f6d1
Riley McCann <rileymccann01@gmail.com>
2019-01-13 02:23:31 -0500

    I hate git

---
+++ Assets/Scripts/MoveCup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCup : MonoBehaviour
+{
+	public float speed = 5f;
+	Rigidbody2D	cup;
+	Vector3 movement;
+	
+    void Awake()
+    {
+        cup = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+		Move(move);
+    }
+	
+	void Move(float dir) {
+		movement.Set(dir, 0, 0);
+		movement = movement.normalized * speed * Time.deltaTime;
+		cup.MovePosition(transform.position + movement);
+	}
+}
