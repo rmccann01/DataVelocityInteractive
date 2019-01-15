29aa1b0dd212d03fd48674f7a0ca3e58ebc12d84
Nishant Chintala <crnishant@gmail.com>
2019-01-16 12:56:04 +1300

    mosaic working

---
+++ Assets/Scripts/UpdateMosaic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateMosaic : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] mosaics;
+    // Update is called once per frame
+    public void UpdateImage(int balls)
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = mosaics[balls];
+    }
+}
