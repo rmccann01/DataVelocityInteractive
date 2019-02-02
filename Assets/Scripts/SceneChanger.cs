f5fef60a4484ea3725e2055302e6d6cc2f0e0e19
rmccann01 <31964312+rmccann01@users.noreply.github.com>
2019-02-02 00:49:10 -0500

    Finalizing comments

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -7,15 +7,14 @@ using UnityEngine.UI;
 public class SceneChanger : MonoBehaviour
 {
     public Button startButton;
-
+    //Add listener for buttons
     void Start()
     {
         startButton.onClick.AddListener(OnClick);
     }
-
+    //Load game from first menu on click
     void OnClick()
     {
         SceneManager.LoadScene("SampleScene");
-        //SceneManager.UnloadSceneAsync("IntroScene");
     }
 }
