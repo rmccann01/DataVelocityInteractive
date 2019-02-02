68470a2c2f1f89b3b630825934d061db555bab9d
rmccann01 <31964312+rmccann01@users.noreply.github.com>
2019-02-02 00:47:33 -0500

    Finalizing comments

--- Assets/Scripts/RemoveDataBot.cs
+++ Assets/Scripts/RemoveDataBot.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class RemoveDataBot : MonoBehaviour
 {
-	public int missedData = 0;
+    public int missedData = 0;
+    //Remove data when it reaches bottom of screen
     private void OnTriggerEnter2D(Collider2D other) {
-		Destroy(other.gameObject);
-		missedData++;
-	}
+	Destroy(other.gameObject);
+	missedData++;
+    }
 }
