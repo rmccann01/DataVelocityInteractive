fe0aff961aa9f481fa6dfd8d4890f356f4074d74
rmccann01 <31964312+rmccann01@users.noreply.github.com>
2019-02-02 00:46:40 -0500

    Finalizing comments

--- Assets/Scripts/MoveCup.cs
+++ Assets/Scripts/MoveCup.cs
@@ -8,18 +8,18 @@ public class MoveCup : MonoBehaviour
     public float xMin, xMax; //Created to set the cup boundaries in Unity
     Rigidbody2D cup;
     Vector3 movement;
-
+    //Initialize cup's rigidbody
     void Awake()
     {
         cup = GetComponent<Rigidbody2D>();
     }
-
+    //move cup per frame horizontally
     void FixedUpdate()
     {
         float move = Input.GetAxisRaw("Horizontal");
         Move(move);
     }
-
+    //move cup based on input from update
     void Move(float dir)
     {
         Vector3 updatedMovement; //Created to clamp the movement
