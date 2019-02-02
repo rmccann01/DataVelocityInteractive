04b2508b914104eb0cd473ca00097bea4d9e85e5
rmccann01 <31964312+rmccann01@users.noreply.github.com>
2019-02-02 00:59:47 -0500

    Finalizing comments

--- Assets/Scripts/UpdateMosaic.cs
+++ Assets/Scripts/UpdateMosaic.cs
@@ -7,17 +7,17 @@ public class UpdateMosaic : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     public Sprite[] mosaics;
     public static int level = 0;
-
+    //Initialize spriteRenderer of Image output
     public void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
-    // Update is called once per frame
+    //Display proper image based on data caught
     public void UpdateImage(int balls)
     {
         spriteRenderer.sprite = mosaics[(level*10)+balls];
     }
-
+    //Reset image at end of game
     public void ResetImage()
     {
         level++;
@@ -28,7 +28,7 @@ public class UpdateMosaic : MonoBehaviour
         spriteRenderer.sprite = mosaics[0];
         
     }
-
+    //Make image black for new Level
     public void ClearImage()
     {
         spriteRenderer.sprite = mosaics[0];
